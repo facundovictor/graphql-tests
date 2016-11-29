978bb7bc55857a3611548624486823bcb8343619
Facundo Victor <facundovt@gmail.com>
2016-11-29 20:11:31 -0300

    Add list type chapter

---
+++ list-type/index.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { graphql, buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+
+type Video {
+   id: ID,
+   title: String,
+   duration: Int,
+   watched: Boolean
+}
+
+type Query {
+  video: Video
+  videos: [Video]
+}
+
+type Schema {
+   query: Query
+}
+
+`);
+
+
+const videoA = {
+  id: 'a',
+  title: 'Create a GraphQL Schema',
+  duration: 120,
+  watched: true
+};
+
+const videoB = {
+  id: 'b',
+  title: 'Ember.js CLI',
+  duration: 240,
+  watched: false
+};
+const videos = [videoA, videoB]
+
+const resolvers = {
+  video: () => ({
+    id: '1',
+    title: 'bar',
+    duration: 180,
+    watched: true
+  }),
+  videos: () => videos
+};
+
+const query = `
+query myFirstQuery {
+  videos {
+    id,
+    title,
+    duration,
+    watched
+  }
+}
+`;
+
+
+graphql(schema, query, resolvers)
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
+
